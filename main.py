ff0354df84a21c5992c11dc74a0aba0546cb344c
tomflood29 <tomflood@college.harvard.edu>
2026-05-04 19:37:13 +0000

    Mon, May 4, 2026, 3:37 PM -04:00

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fetch_data3 import fetch_live_prices, fetch_historical_prices
-from map_viz3 import build_map
+from fetch_data import fetch_live_prices, fetch_historical_prices
+from map_viz import build_map
 
 prices = fetch_live_prices()
 snapshots  = fetch_historical_prices(hours_back=24)
-print("historical frames:", len(snapshots))
-print("historical labels:", list(snapshots.keys())[:5], "...")
 build_map(prices, snapshots=snapshots)
